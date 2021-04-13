21adad1274a15e7f71cabe260befc1501b8166c0
isrt09 <mazedur@gmail.com>
2021-04-13 22:07:45 +0600

    Non Spread Operator

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ const poor    = ['BAN','BHU','AFS'];
 const rich    = [...poor,'USA','UK','GER'];
 console.log(rich);
 console.log(Object.entries(rich));
+
+// Without Spread Operator
+
+let south = ['Khula','Barisal','Sathkhira'];
+let north = ['Bogra','Rangpur','Dinajpur'];
+
+console.log(north.push(south));
