b7b528bdf9c1bdac839c0235bc2a3fb4500518d5
isrt09 <mazedur@gmail.com>
2021-04-13 22:26:58 +0600

    Rest operator

--- app.js
+++ app.js
@@ -11,3 +11,15 @@ let south = ['Khula','Barisal','Sathkhira'];
 let north = ['Bogra','Rangpur','Dinajpur'];
 
 console.log(north.push(south));
+
+
+// Rest Operator
+function calculation(a,b,...etc){
+    let sum = 0;
+    for(let i of etc){
+        //sum +=i; 
+        sum = sum + i;
+    }    
+    console.log(sum);
+}
+calculation(10,20,5,5,5);
