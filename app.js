20c6e88065c5f919aa08ed9c11d8a16968ed9d56
isrt09 <mazedur@gmail.com>
2021-04-14 15:39:19 +0600

    All Functions

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // Spread Operator
 
+
 const poor    = ['BAN','BHU','AFS'];
 const rich    = [...poor,'USA','UK','GER'];
 console.log(rich);
@@ -119,3 +120,121 @@ const GYM = {
 for(let member in GYM){
     console.log(member+":"+GYM[member]);
 }
+
+
+// Functions 
+
+// No Parameter
+function add(){
+    return 'Please do some calculation';
+}
+console.log(add());
+
+// With Parameter
+function calculation(weight, height){
+    return weight/height;
+}
+let BMI = calculation(85,124);
+console.log("Your BMI is "+BMI);
+
+//Rest Parameter
+function price(...etc){
+    console.log(etc[2,4,5]);
+}
+price(1,2,1,21,1,31,12,1,1,5)
+
+// Return means receive value whatever it has or not ....
+
+function goOffice(){
+    return 'Welcome to my Sweat Home!';
+}
+function backHome(){
+    return goOffice();
+}
+console.log(backHome());
+
+// Anonymous Function
+
+var myName = function(){
+    return 'Welcome to JS';
+}
+console.log(myName());
+
+// Arrow Function
+
+// use curly braceket and no parameter
+let myCountry = ()=>{
+    console.log('Welcome to Bangladesh');
+}
+
+// no curly bracket
+let myVillage = () => console.log('Welcome to Instructory');
+myVillage();
+
+// with parameter neeb to be curly bracket to work N.B. multiple parameter no works except curly bracket
+let visa = (passport,ticket) => {
+    console.log('Passport No :'+passport+'\nTicket No :'+ticket);
+}
+visa('A2013B23', 'Economy Class EC 2031');
+
+// Arrow with Rest
+let river = (river_1,river_2,...others)=>{
+    console.log(river_1,river_2);
+}
+river('Padma','Jamuna','Meghna','Buriganda'); // You will not see all but only 2 
+
+// Arrow with Return
+
+let home = (address)=>{
+    return address;
+}
+console.log(home('Noakhali'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
